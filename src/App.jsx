a710d3725cb397d2123e61dd4b8b6fdd142f045b
AnaCe-7 <ana.cecilia.reques@gmail.com>
2024-03-19 19:14:22 +0100

    Create Router files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
+import './App.css'
+import Router from './configuration/Router.jsx';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Router/>
+   </>
+  );
 }
+
+export default App;
---
+++ src/config/Router.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import React from 'react'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      
+        {/* <Route path="/" element={}></Route> 
+        <Route path="/home" element={}></Route>
+        <Route path="/allartworks" element={}></Route>
+        <Route path="/artworks" element={}></Route>
+        <Route path="/createartwork" element={}></Route> 
+        */}
+
+    </Routes>
+</BrowserRouter>
+  )
+}
+
+export default Router
